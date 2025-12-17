5c43c71fb58fd5c8938449828b2f6968ebce4de4
MabusTFC <vanechka.smirnov.04@inbox.ru>
2025-12-17 14:11:51 +0500

    Feat: Добавил тестовые данные Fix: Мелкие фиксы

--- app/src/main/java/com/example/calendar_android_app/MainActivity.kt
+++ app/src/main/java/com/example/calendar_android_app/MainActivity.kt
@@ -5,22 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calendar_android_app.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(binding.root)
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, MainFragment())
             .commit()
     }
 }
-
-
-
-
-
-
-
--- app/src/main/java/com/example/calendar_android_app/model/WeekService.kt
+++ app/src/main/java/com/example/calendar_android_app/model/WeekService.kt
@@ -38,48 +38,67 @@ object WeekService {
     }
 
 
-    fun getOddWeekSchedule(): Week {
+    private fun getOddWeekSchedule(): Week {
         return Week(
             type = WeekParity.ODD,
             days = listOf(
-                Day(
-                    dayOfWeek = DayOfWeek.MONDAY,
-                    lessons = listOf(
-                        Lesson(
-                            startTime = LocalTime.of(8, 30),
-                            endTime = LocalTime.of(10, 5),
-                            subject = "Математический анализ"
-                        ),
-                        Lesson(
-                            startTime = LocalTime.of(10, 20),
-                            endTime = LocalTime.of(11, 55),
-                            subject = "Программирование"
-                        )
-                    )
-                ),
+                Day(DayOfWeek.MONDAY, listOf(
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Озд. физ. культура (Лек.)"),
+                    Lesson(LocalTime.of(18, 50), LocalTime.of(20, 20), "Базы и хранилища данных (Лек.)")
+                )),
+                Day(DayOfWeek.TUESDAY, listOf(
+                    Lesson(LocalTime.of(18, 50), LocalTime.of(20, 20), "Упр. IT-проектами (Лек.)"),
+                    Lesson(LocalTime.of(20, 30), LocalTime.of(22, 0), "Упр. IT-проектами (Пр.)")
+                )),
+                Day(DayOfWeek.THURSDAY, listOf(
+                    Lesson(LocalTime.of(13, 50), LocalTime.of(15, 20), "Интернет-маркетинг (Лек.)"),
+                    Lesson(LocalTime.of(15, 30), LocalTime.of(17, 0), "Интернет-маркетинг (Пр.)"),
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Интернет-маркетинг (Пр.)")
+                )),
+                Day(DayOfWeek.FRIDAY, listOf(
+                    Lesson(LocalTime.of(15, 30), LocalTime.of(17, 0), "Архитектура предприятия (Лек.)"),
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Архитектура предприятия (Пр.)")
+                )),
+                Day(DayOfWeek.SATURDAY, listOf(
+                    Lesson(LocalTime.of(8, 30), LocalTime.of(10, 0), "Анализ данных (Лек.)"),
+                    Lesson(LocalTime.of(15, 30), LocalTime.of(17, 0), "Техн. приклад. прогр-ия (Лек.)"),
+                    Lesson(LocalTime.of(15, 30), LocalTime.of(17, 0), "Создание бизнес-прилож. (Лек.)")
+                ))
             )
         )
     }
 
-    fun getEvenWeekSchedule(): Week {
+    private fun getEvenWeekSchedule(): Week {
         return Week(
             type = WeekParity.EVEN,
             days = listOf(
-                Day(
-                    dayOfWeek = DayOfWeek.MONDAY,
-                    lessons = listOf(
-                        Lesson(
-                            startTime = LocalTime.of(8, 30),
-                            endTime = LocalTime.of(10, 5),
-                            subject = "Математический анализ"
-                        ),
-                        Lesson(
-                            startTime = LocalTime.of(10, 20),
-                            endTime = LocalTime.of(11, 55),
-                            subject = "Программирование"
-                        )
-                    )
-                ),
+                Day(DayOfWeek.MONDAY, listOf(
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Озд. физ. культура (Лек.)"),
+                    Lesson(LocalTime.of(18, 50), LocalTime.of(20, 20), "Базы и хранилища данных (Пр.)"),
+                    Lesson(LocalTime.of(20, 30), LocalTime.of(22, 0), "Базы и хранилища данных (Пр.)")
+                )),
+                Day(DayOfWeek.TUESDAY, listOf(
+                    Lesson(LocalTime.of(18, 50), LocalTime.of(20, 20), "Упр. IT-проектами (Лек.)"),
+                    Lesson(LocalTime.of(20, 30), LocalTime.of(22, 0), "Упр. IT-проектами (Пр.)")
+                )),
+                Day(DayOfWeek.WEDNESDAY, listOf(
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Интернет-технологии (Лек.)"),
+                    Lesson(LocalTime.of(18, 50), LocalTime.of(20, 20), "Интернет-технологии (Пр.)"),
+                    Lesson(LocalTime.of(20, 30), LocalTime.of(22, 0), "Интернет-технологии (Пр.)")
+                )),
+                Day(DayOfWeek.FRIDAY, listOf(
+                    Lesson(LocalTime.of(15, 30), LocalTime.of(17, 0), "Архитектура предприятия (Лек.)"),
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Архитектура предприятия (Пр.)"),
+                    Lesson(LocalTime.of(18, 50), LocalTime.of(20, 20), "Базы и хранилища данных (Пр.)")
+                )),
+                Day(DayOfWeek.SATURDAY, listOf(
+                    Lesson(LocalTime.of(12, 10), LocalTime.of(13, 40), "Анализ данных (Пр.)"),
+                    Lesson(LocalTime.of(13, 50), LocalTime.of(15, 20), "Анализ данных (Пр.)"),
+                    Lesson(LocalTime.of(15, 30), LocalTime.of(17, 0), "Техн. приклад. прогр-ия (Пр.)"),
+                    Lesson(LocalTime.of(15, 30), LocalTime.of(17, 0), "Создание бизнес-прилож. (Пр.)"),
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Техн. приклад. прогр-ия (Пр.)"),
+                    Lesson(LocalTime.of(17, 10), LocalTime.of(18, 40), "Создание бизнес-прилож. (Пр.)")
+                ))
             )
         )
     }
